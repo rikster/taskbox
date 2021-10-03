91231c546940b785d6038a352bcec3fd28d89d2f
Richard Hounslow <richard.hounslow@sportsbet.com.au>
2021-10-03 17:22:51 +1100

    smashed out remaining code

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import TaskList from './TaskList';
+import TaskList from "./TaskList";
 
 export function PureInboxScreen({ error }) {
     if (error) {
--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+
 import { action } from "@storybook/addon-actions";
-import {Provider} from 'react-redux';
+
+import { Provider } from "react-redux";
 
 import { PureInboxScreen } from "./InboxScreen";
+import * as TaskListStories from "./TaskList.stories";
 
-import * as TaskListStories from './TaskList.stories';
 export default {
   component: PureInboxScreen,
   title: "InboxScreen",
@@ -13,19 +15,20 @@ export default {
 
 // A super-simple mock of a redux store
 const store = {
-    getState: () => {
-        return {
-            tasks: TaskListStories.Default.args.tasks,
-        };
-    },
-    subscribe: () => 0,
-    dispatch: action('dispatch'),
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args.tasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action("dispatch"),
 };
-const Template = args => <PureInboxScreen {...args} />;
+
+const Template = (args) => <PureInboxScreen {...args} />;
 
 export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = {
-    error: 'Something',
+  error: "Something",
 };
